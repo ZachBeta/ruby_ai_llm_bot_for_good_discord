35863a0d0b614fedb21d4a327ecdf3ad92a9f14a
Zach Morek <zmorek@gmail.com>
2025-03-03 12:29:03 -0500

    move rails up to root dir

--- rail_bfg_d/config/initializers/discord_bot.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.after_initialize do
-  # Make sure the Discord bot services are loaded
-  Rails.autoloaders.main.eager_load_dir(Rails.root.join('app/services/discord_bot'))
-end 
